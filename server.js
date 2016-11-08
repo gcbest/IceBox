b5eea075286d09246f22038130ce1ebe4cc0fd7a
gcbest <godfrey.c.best@gmail.com>
2016-11-07 20:25:04 -0500

    baseline

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3005;
+
+
+app.get('/', function(req, res) {
+
+});

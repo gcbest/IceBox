c96916ecfaef2121a7978b6edd0814576a8711a8
gcbest <godfrey.c.best@gmail.com>
2016-12-30 14:26:56 -0500

    server listening

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+var PORT = process.env.PORT;
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '/app/views/index.html'))
+});
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
 });

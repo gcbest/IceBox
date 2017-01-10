b7f1c3772922a8ec0fee9b7c3a486b650433f78b
gcbest <godfrey.c.best@gmail.com>
2017-01-09 21:04:50 -0500

    twitter width updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/app/public'));
 
